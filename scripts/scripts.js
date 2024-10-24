1a19c8a804508a7a2201ee6e736526b311d505a2
TN <thiendang01031994@gmail.com>
2024-10-24 17:50:47 -0400

    ADD GETHUMANCHOICE FUNCTION
    Logic for humanchoice func added. Prompts user
    for recognized input strings only (rock, paper or
    scissors). Handling added for cancellation of
    prompt box.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,14 +7,15 @@ function getComputerChoice(){
     let randomGen = Math.floor(Math.random()*30);
     let compChoice;
     //console.log(randomGen);
-    if(randomGen > 10 & randomGen <= 20){
+    if (randomGen > 10 & randomGen <= 20){
         compChoice = "Rock";
-    }
-    else if(randomGen >= 21){
-        compChoice = "Scissors";
-    }
-    else{
+
+    } else if (randomGen > 20){
         compChoice = "Paper";
+
+    } else {
+
+        compChoice = "scissors";
     }
 
     return compChoice;
@@ -26,6 +27,28 @@ function getComputerChoice(){
 // create variable to store user's selection
 // prompt user and save user's selection to the variable
 // logic for caseinsensitive
+function getHumanChoice(){
+    // didnt set the initial string to null because I needed null values to be used for canceling the prompt() only.
+    let humanChoice = "Initial String";
+
+    let i = 0;
+    while (i == 0) {
+        // this checks for user's ESC or closing the prompt
+        if (humanChoice === null) {
+            console.log("user canceled the prompt")
+            return null;
+        }
+        // looks for rock, paper, scissors strings only. Will continue to prompt user until the strings are recognized or prompt cancelled.
+        if (humanChoice.toLowerCase() === "rock" || humanChoice.toLowerCase() === "paper" || humanChoice.toLowerCase() === "scissors") {
+            i = 1;
+            return humanChoice.toLowerCase();
+
+        } else {
+            humanChoice = prompt();
+
+        }
+    } 
+}
 
 
 // create function to evaluate comp vs human selections called playRound
