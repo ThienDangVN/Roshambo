3ca8ca6665127972fd6b70af034c749ad43e3bd7
TN <thiendang01031994@gmail.com>
2024-10-28 17:39:48 -0400

    ALGORITHM FOR PLAYGAME ADD SOME ERROR HANDLING
    Logic for playing the entire game. add error
    handling for user cancelling the prompt.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -39,35 +39,33 @@ function getHumanChoice(){
         }
         // looks for rock, paper, scissors strings only. Will continue to prompt user until the strings are recognized or prompt cancelled.
         if (usersInput.toLowerCase() === "rock" || usersInput.toLowerCase() === "paper" || usersInput.toLowerCase() === "scissors") {
-            i = 1;
+            // i = 1;
             return usersInput.toLowerCase();
 
         } else {
-            usersInput = prompt();
+            usersInput = prompt("Please Enter \"Rock\", \"Paper\" Or \"Scissors\" Only");
 
         }
     } 
 }
 
-let humanScore = 0;
-let computerScore = 0;
+
 
 // create function to evaluate comp vs human selections called playRound
 // playRound Function has two arguments: humanChoice and computerChoice.
 // IF comp selection is greater than user's write to console "You lose! Paper Beats Rock" ELSE you win
 
+let humanScore = 0;
+let computerScore = 0;
+
 function playRound(humanChoice, computerChoice) {
 
-
-    // if (humanChoice === null) {
-    //     console.log("No User Selection, GAMEOVER.")
-    // } else if (humanChoice === "rock" & computerChoice === "scissors") {
-    //     console.log("human win")
-    // }
-
+    // switch to evaluate user's selection vs comp
     switch(humanChoice) {
+        // null case is used for catching prompt cancellation.
         case null:
-            console.log("No User Selection, GAMEOVER")
+            console.log("No User Selection")
+            humanScore = null;
             break;
         
         case "rock":
@@ -81,13 +79,58 @@ function playRound(humanChoice, computerChoice) {
                 console.log("Draw!")
             }
             break;
+        
+        case "paper":
+            if (computerChoice === "Rock") {
+                humanScore ++;
+                console.log("You Win! paper Beats Rock");
+            } else if (computerChoice === "scissors") {
+                computerScore ++;
+                console.log("You Lose! Scissors Beats Paper")
+            } else {
+                console.log("Draw!")
+            }
+            break;
+        
+        case "scissors":
+            if (computerChoice === "Paper") {
+                humanScore ++;
+                console.log("You Win! Scissors Beat Paper");
+            } else if (computerChoice === "Rock") {
+                computerScore ++;
+                console.log("You Lose! Rock Beats Scissors")
+            } else {
+                console.log("Draw!")
+            }
+            break;
     }
     
-    console.log(humanChoice, computerChoice);
+    // console.log(humanChoice, computerChoice);
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
-console.log(humanScore, computerScore);
+function playGame() {
+    
+    // while loop runs while number of rounds is less than 5. 
+    // null check for prompt cancellation. breaks out of loop if null.
+    let roundNumber = 0;
+    while (roundNumber < 5) {
+
+        playRound(getHumanChoice(), getComputerChoice())
+        if (humanScore === null) {
+            // roundNumber = 5;
+            break;
+        }
+        roundNumber++;
+    }
+    console.log(`User\'s Score: ${humanScore}`, `Computer\'s Score: ${computerScore}`);
+
+}
+
+playGame();
+
+if (humanScore > computerScore & humanScore !== null) {
+    console.log("Congraduations! You Won!")
+} else {
+    console.log("You Lose, Game Over")
+}
