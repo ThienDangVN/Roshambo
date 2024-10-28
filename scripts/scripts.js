f60f683dba29705f7d23ca6ad43e2afbd1c7a8d5
TN <thiendang01031994@gmail.com>
2024-10-28 16:27:30 -0400

    ADD LOGIC FOR PLAYING ONE ROUND OF THE GAME
    Switch statements to evaluate the user's vs comp
    selections.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -21,7 +21,6 @@ function getComputerChoice(){
     return compChoice;
 }
 
-//console.log(getComputerChoice());
 
 // create function name for getHumanChoice
 // create variable to store user's selection
@@ -29,28 +28,66 @@ function getComputerChoice(){
 // logic for caseinsensitive
 function getHumanChoice(){
     // didnt set the initial string to null because I needed null values to be used for canceling the prompt() only.
-    let humanChoice = "Initial String";
+    let usersInput = "Initial String";
 
     let i = 0;
     while (i == 0) {
         // this checks for user's ESC or closing the prompt
-        if (humanChoice === null) {
-            console.log("user canceled the prompt")
+        if (usersInput === null) {
+            //console.log("user canceled the prompt")
             return null;
         }
         // looks for rock, paper, scissors strings only. Will continue to prompt user until the strings are recognized or prompt cancelled.
-        if (humanChoice.toLowerCase() === "rock" || humanChoice.toLowerCase() === "paper" || humanChoice.toLowerCase() === "scissors") {
+        if (usersInput.toLowerCase() === "rock" || usersInput.toLowerCase() === "paper" || usersInput.toLowerCase() === "scissors") {
             i = 1;
-            return humanChoice.toLowerCase();
+            return usersInput.toLowerCase();
 
         } else {
-            humanChoice = prompt();
+            usersInput = prompt();
 
         }
     } 
 }
 
+let humanScore = 0;
+let computerScore = 0;
 
 // create function to evaluate comp vs human selections called playRound
 // playRound Function has two arguments: humanChoice and computerChoice.
 // IF comp selection is greater than user's write to console "You lose! Paper Beats Rock" ELSE you win
+
+function playRound(humanChoice, computerChoice) {
+
+
+    // if (humanChoice === null) {
+    //     console.log("No User Selection, GAMEOVER.")
+    // } else if (humanChoice === "rock" & computerChoice === "scissors") {
+    //     console.log("human win")
+    // }
+
+    switch(humanChoice) {
+        case null:
+            console.log("No User Selection, GAMEOVER")
+            break;
+        
+        case "rock":
+            if (computerChoice === "scissors") {
+                humanScore ++;
+                console.log("You Win! Rock Beats Scissors");
+            } else if (computerChoice === "Paper") {
+                computerScore ++;
+                console.log("You Lose! Paper Beats Rock")
+            } else {
+                console.log("Draw!")
+            }
+            break;
+    }
+    
+    console.log(humanChoice, computerChoice);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log(humanScore, computerScore);
